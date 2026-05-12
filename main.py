d57f965ffacffee430934e2dcdfee709e8866496
zixgggg <zixzix035@gmail.com>
2026-05-12 22:07:33 +0800

    add background color,font color,window padding and fix text didn't change bug

--- main.py
+++ main.py
@@ -5,41 +5,27 @@ import configparser
 dpg.create_context()
 config = configparser.ConfigParser()
 config.read("config.ini")
-all_cmd=""
+all_cmd_output=""
 
 with dpg.window(tag="win"):
     with dpg.group(horizontal=True):
-        dpg.add_text(all_cmd,tag="all_cmd_tag")
+        dpg.add_text(all_cmd_output,tag="all_cmd_tag")
         #dpg.add_spacer(width=20)
         #dpg.add_text(i,tag="cpu_tag")
 
-for block in config.sections():#取得所有區塊的特定區塊
-    cmd_to_run = config.get(block, "command", fallback="")
-    cmd_label=config.get(block,"label",fallback="")
-    bar_width=int(config.get("sigma_bar","width",fallback=1920))
-    bar_height=int(config.get("sigma_bar","height",fallback=30))
-    bar_x=int(config.get("sigma_bar","x",fallback=0))
-    bar_y=int(config.get("sigma_bar","y",fallback=0))
-    bar_split_sign=config.get("sigma_bar","split_sign",fallback=" | ")
-    text_font=config.get("sigma_bar","font",fallback="")
-    text_size=int(config.get("sigma_bar","font_size",fallback=20))
-    #cmd=subprocess.check_output(config[block]["command"],shell=True).decode("utf-8").strip()
-    cmd=subprocess.check_output(cmd_to_run,shell=True).decode("utf-8").strip()
-    with dpg.theme() as global_theme:
-        with dpg.theme_component(dpg.mvAll):
-            dpg.add_theme_color(dpg.mvThemeCol_FrameBg,value=(0,147,0),category=dpg.mvThemeCat_Core)
-    dpg.bind_theme(global_theme) 
-    """
-    取得所有區塊的所有值：
-    for block in config.sections():#取得所有區塊的特定區塊
-        keys=config.options(block)#取得特定區塊所有鍵
-        for key in keys:
-            value=config[block][key]#取得特定鍵下的值
-    """
-    if cmd_to_run=="":
-        all_cmd=all_cmd+cmd_label+cmd
-    else:
-        all_cmd=all_cmd+cmd_label+cmd+" | "
+bar_width=int(config.get("sigma_bar","width",fallback=1920))
+bar_height=int(config.get("sigma_bar","height",fallback=30))
+bar_x=int(config.get("sigma_bar","x",fallback=0))
+bar_y=int(config.get("sigma_bar","y",fallback=0))
+bar_split_sign=config.get("sigma_bar","split_sign",fallback="   |   ")
+text_font=config.get("sigma_bar","font",fallback="")
+text_size=int(config.get("sigma_bar","font_size",fallback=20))
+with dpg.theme() as global_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_WindowBg,value=(0,147,0),category=dpg.mvThemeCat_Core)
+        dpg.add_theme_color(dpg.mvThemeCol_Text,value=(0,0,0),category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_WindowPadding,x=0,y=0)
+dpg.bind_theme(global_theme)
 if(text_font!=""):
     with dpg.font_registry():
         with dpg.font(text_font, int(text_size)) as text_font_id:
@@ -59,9 +45,24 @@ dpg.create_viewport(title="sigma_bar",resizable=False,x_pos=bar_x,y_pos=bar_y,wi
 dpg.setup_dearpygui()
 dpg.show_viewport()
 while dpg.is_dearpygui_running():
-    
-    dpg.set_value("all_cmd_tag",all_cmd)
-
+    for block in config.sections():#取得所有區塊的特定區塊
+        cmd_to_run = config.get(block, "command", fallback="")
+        cmd_label=config.get(block,"label",fallback="")
+        #cmd=subprocess.check_output(config[block]["command"],shell=True).decode("utf-8").strip()
+        cmd=subprocess.check_output(cmd_to_run,shell=True).decode("utf-8").strip()
+        """
+        取得所有區塊的所有值：
+        for block in config.sections():#取得所有區塊的特定區塊
+            keys=config.options(block)#取得特定區塊所有鍵
+            for key in keys:
+                value=config[block][key]#取得特定鍵下的值
+        """
+        if cmd_to_run=="":
+            all_cmd_output=all_cmd_output+cmd_label+cmd
+        else:
+            all_cmd_output=all_cmd_output+cmd_label+cmd+bar_split_sign
+    dpg.set_value("all_cmd_tag",all_cmd_output)
+    all_cmd_output=""
     print("this will run every frame")
     dpg.render_dearpygui_frame()
     time.sleep(0.5)
