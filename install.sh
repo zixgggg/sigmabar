77747eeb806885c8e738f6f8df7f7b96a5cad0ab
zixgggg <zixzix035@gmail.com>
2026-06-05 14:59:49 +0800

    add install method comment

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+#curl https://raw.githubusercontent.com/zixgggg/sigmabar/refs/heads/main/install.sh
 git clone https://github.com/zixgggg/sigmabar.git
 cd sigmabar/
 cp sigmabar $HOME/.local/bin
