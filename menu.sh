fc69c1044ba8409382ce22a821e45eb5263092ff
zixgggg <zixzix035@gmail.com>
2026-06-13 16:19:57 +0800

    add echo message

--- menu.sh
+++ menu.sh
@@ -4,6 +4,9 @@ echo "(2) uninstall sigma_bar"
 read -p "enter number:" i < /dev/tty
 if [ "$i" = "1" ];then
 	./install.sh
+	echo "installing"
 elif [ "$i" = "2" ];then
 	./uninstall.sh
+	echo "uninstalling"
+
 fi
