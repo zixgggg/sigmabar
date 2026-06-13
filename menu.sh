1cc10f4e571dd245ab176d121900ab99a54cb92b
zixgggg <zixzix035@gmail.com>
2026-06-13 16:03:53 +0800

    add "< /dev/tty" at input end let curl can work

--- menu.sh
+++ menu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 echo -e "(1) install sigma_bar"
 echo -e "(2) uninstall sigma_bar"
-read -p "enter number:" i
+read -p "enter number:" i < /dev/tty
 if [ "$i" = "1" ];then
 	./install.sh
 elif [ "$i" = "2" ];then
