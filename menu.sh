3fc00e4459a748834499d31be88b3a5bb1f84bfa
zixgggg <zixzix035@gmail.com>
2026-06-13 19:10:46 +0800

    pipe to bash not sh

--- menu.sh
+++ menu.sh
@@ -4,11 +4,11 @@ echo "(2) uninstall sigma_bar"
 read -p "enter number:" i < /dev/tty
 if [ "$i" = "1" ];then
 	#./install.sh
-	curl -fsSL https://raw.githubusercontent.com/zixgggg/sigmabar/refs/heads/main/install.sh|sh
+	curl -fsSL https://raw.githubusercontent.com/zixgggg/sigmabar/refs/heads/main/install.sh|bash
 	echo "installing"
 elif [ "$i" = "2" ];then
 	#./uninstall.sh
-	curl -fsSL https://raw.githubusercontent.com/zixgggg/sigmabar/refs/heads/main/uninstall.sh|sh
+	curl -fsSL https://raw.githubusercontent.com/zixgggg/sigmabar/refs/heads/main/uninstall.sh|bash
 	echo "uninstalling"
 
 fi
