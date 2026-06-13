470d56bec48e241bdd80f6e745492d2f3ef262c6
zixgggg <zixzix035@gmail.com>
2026-06-13 16:24:20 +0800

    add curl to install and uninstall

--- menu.sh
+++ menu.sh
@@ -3,10 +3,12 @@ echo "(1) install sigma_bar"
 echo "(2) uninstall sigma_bar"
 read -p "enter number:" i < /dev/tty
 if [ "$i" = "1" ];then
-	./install.sh
+	#./install.sh
+	curl -fsSL https://raw.githubusercontent.com/zixgggg/sigmabar/refs/heads/main/install.sh|sh
 	echo "installing"
 elif [ "$i" = "2" ];then
-	./uninstall.sh
+	#./uninstall.sh
+	curl -fsSL https://raw.githubusercontent.com/zixgggg/sigmabar/refs/heads/main/uninstall.sh|sh
 	echo "uninstalling"
 
 fi
