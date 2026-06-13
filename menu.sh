01451fc085c9261435f6ed822c7f371588e90d93
zixgggg <zixzix035@gmail.com>
2026-06-13 16:14:04 +0800

    rm -e from echo

--- menu.sh
+++ menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-echo -e "(1) install sigma_bar"
-echo -e "(2) uninstall sigma_bar"
+echo "(1) install sigma_bar"
+echo "(2) uninstall sigma_bar"
 read -p "enter number:" i < /dev/tty
 if [ "$i" = "1" ];then
 	./install.sh
